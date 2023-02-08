02e6b8ab03a55ad80dcbeb4b0b3b26ed2c0c390a
Cabra98 <102603446+Cabra98@users.noreply.github.com>
2023-02-08 20:03:34 -0300

    Update Rdp.java

--- Los Borbotones-TpFinal-ProgConcurrente.(CÓDIGO)/src/Rdp.java
+++ Los Borbotones-TpFinal-ProgConcurrente.(CÓDIGO)/src/Rdp.java
@@ -5,24 +5,21 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
 
 public class Rdp {
 
     private Integer [][] incidenciaReal = new Integer[14][10];                  // incidencia positiva - incidencia negativa
-    private Integer [][] incidenciaRealPositiva = new Integer[14][10];
-    private Integer [][] incidenciaRealNegativa = new Integer[14][10];
+    private Integer [][] incidenciaPositiva = new Integer[14][10];
+    private Integer [][] incidenciaNegativa = new Integer[14][10];
     private Integer []   marcaInicial = new Integer[14];
     private Integer []   marcaActual  = new Integer[14];
+  
 
 
-    private int columnas;
-    private int filas;
-
-
+    /**
+     * Crea un objeto Red de Petri (Rdp) y carga las matrices de incidencia positiva, incidencia negativa
+     * y el vector con la marca inicial de la red.
+     */
     public Rdp(){
         try {
             cargarMatriz("forward"); //Cargamos matriz forward
@@ -51,6 +48,12 @@ public class Rdp {
     }
 
 
+    /**
+     * Lee los archivos .txt y carga los valores a la matriz de incidencia positivo o negativo segun
+     * el parametro ("foward" para la positiva, backward para la negativa)
+     * @param tipoMatriz
+     * @throws FileNotFoundException
+     */
     private void cargarMatriz(String tipoMatriz) throws FileNotFoundException {
 
         Scanner input = null;
@@ -78,25 +81,35 @@ public class Rdp {
 
         if(tipoMatriz.equals("forward")) {
 
-            incidenciaRealPositiva = auxiliar;
+            incidenciaPositiva = auxiliar;
         }else{
-            incidenciaRealNegativa = auxiliar;
+            incidenciaNegativa = auxiliar;
         }
 
     }
 
+
+    /**
+     * Resta las matrices de incidencia positiva y negativa para obtener la matriz de incidencia real
+     * y la devuelve.
+     * @return
+     */
     private Integer[][] restarMatrices() {
 
         Integer[][] result = new Integer[14][10];
             for (int i = 0; i < 14; i++) {
                 for (int j = 0; j < 10; j++) {
-                    result[i][j] = incidenciaRealPositiva[i][j] - incidenciaRealNegativa[i][j];
+                    result[i][j] = incidenciaPositiva[i][j] - incidenciaNegativa[i][j];
                 }
             }
             return result;
         }
 
 
+    /**
+     * Lee el archivo .txt y lo carga en la variable marca inicial
+     * @throws FileNotFoundException
+     */
     private void cargarMarcaInicial() throws FileNotFoundException{
 
         Scanner input = null;
@@ -116,6 +129,13 @@ public class Rdp {
 
 
     }
+
+
+    /**
+     * realiza el disparo de la transicion pasada por parametro, siempre y cuando la transicion
+     * este sensibilizada.
+     * @param transicion
+     */
 
     private void disparar(int transicion){
 
@@ -147,14 +167,18 @@ public class Rdp {
 
     }
 
-
+    /**
+     * Indica si la transicion pasada por parametro esta sensibilizada y lista para ser disparada
+     * @param transicion
+     * @return
+     */
     private boolean estaSensibilizada(int transicion){
         ArrayList<Integer> plazasSensibilizantes = new ArrayList<Integer>();
 
 
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < 14; j++) {
 
-                if(incidenciaRealNegativa[j][transicion-1]  == 1){
+                if(incidenciaNegativa[j][transicion-1]  == 1){
 
                     plazasSensibilizantes.add(j);
                 }
@@ -172,7 +196,12 @@ public class Rdp {
 
     }
 
-
+    /**
+     * Multiplica las matrices pasadas como parametro y devuelve el resultado
+     * @param a
+     * @param b
+     * @return
+     */
     private Integer[] multiplicarMatrices(Integer[][] a, Integer[] b) {
 
         Integer[] result = new Integer[14];
@@ -188,6 +217,13 @@ public class Rdp {
         return result;
     }
 
+
+    /**
+     * Suma las matrices pasadas por parametro y devuelve el resultado
+     * @param a
+     * @param b
+     * @return
+     */
     private Integer[] sumarMatrices(Integer[] a, Integer[] b) {
 
         Integer[] result = new Integer[14];
