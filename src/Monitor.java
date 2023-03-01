ec28530938062e23f8aeb68349812432abd65285
Marco Valentino Di Giannantonio <88598932+Valentino-DG@users.noreply.github.com>
2023-03-01 18:57:59 -0300

    Update Monitor.java

--- src/Monitor.java
+++ src/Monitor.java
@@ -1,36 +1,51 @@
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Monitor {
     private Log log;
     private Rdp red;
+    private Politica politica;
     private ReentrantLock lock;
-    private ArrayList<Condition> colaDeHilos;
-    private ArrayList<Integer>encolados = new ArrayList<Integer>();
-    Random rand = new Random();
-    //private Integer [] encolados = new Integer[10];
-    //Agregar politica
+   // private ArrayList<Condition> colaDeHilos;
+    private ArrayList<Integer> encolados = new ArrayList<Integer>();
+    private ArrayList<Semaphore> colaDeHilos;
+    private Semaphore mutex ;
 
     public Monitor(Log log){
-        this.log = log;
-        this.red = new Rdp();
-        this.lock = new ReentrantLock();
-        this.colaDeHilos = new ArrayList<Condition>();
 
-        for (int i = 0; i < red.getCantTransiciones(); i++) {
-        //Creamos una cola para cada transicion para que almacene los hilos que se encuentren esperando a poder dispararla.
-            colaDeHilos.add(lock.newCondition());
-            }
+        this.log = log;
+        red = new Rdp();
+        politica=new Politica();
+        lock = new ReentrantLock();
+       // this.colaDeHilos = new ArrayList<Condition>();
+        colaDeHilos = new ArrayList<Semaphore>();
+        mutex = new Semaphore(1);
+
+       // for (int i = 0; i < red.getCantTransiciones(); i++) {
+           //Creamos una cola para cada transicion para que almacene los hilos que se encuentren esperando a poder dispararla.
+         // colaDeHilos.add(lock.newCondition());
+        //}
+
+        for(int i = 0; i < red.getCantTransiciones(); i++){
+            colaDeHilos.add(new Semaphore(0));
+        }
+
     }
 
     public void dispararTransicion(Integer transicion){
-        lock.lock();
-        while(!red.disparar(transicion)){
+
+    try {
+        mutex.acquire();
+    } catch(Exception e){
+    }
+
+    while(!red.disparar(transicion)){
             try{
                 encolados.add(transicion);
-                colaDeHilos.get(transicion).await();
+                mutex.release();
+                colaDeHilos.get(transicion-1).acquire();
                 encolados.remove(transicion);
             }
             catch (Exception e){
@@ -40,27 +55,58 @@ public class Monitor {
         }
 
         if(transicion == 6 || transicion == 10 || transicion == 4 || transicion == 5) {
-
             log.logear(transicion);
-
         }
 
-        if(encolados.size() != 0){
+        if(encolados.size() > 0) {
 
-            for (Integer iterador:  encolados) {
+            encolados = politica.reordenar(encolados);
 
-              if(red.estaSensibilizada(iterador)){
+            for (Integer it : encolados) {
+                if (red.estaSensibilizada(it)) {
+                    colaDeHilos.get(it - 1).release();
+                    return;
 
-                    colaDeHilos.get(iterador).signal();
-                    break;
                 }
             }
-
-
         }
-
-
-        lock.unlock();
+            mutex.release();
 
     }
+
+
+
+    private ArrayList<Integer> sensibilizada( ArrayList<Integer> encolados ){
+
+      ArrayList<Integer> encSens = new ArrayList<Integer>();
+
+      for(Integer it: encolados){
+
+          if(red.estaSensibilizada(it)){
+              encSens.add(it);
+          }
+      }
+
+        imprimir(encSens);
+        imprimir(encolados);
+
+      if(encSens.isEmpty()){
+
+          return encolados;
+      }else{
+          return encSens;
+      }
+
+    }
+
+    private void imprimir(ArrayList<Integer> lista){
+
+        for(Integer it: lista){
+
+            System.out.print(it);
+        }
+        System.out.println("--------------------------");
+    }
+
+
 }
